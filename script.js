1ec1ce75091c406449d86bd0988938ee3e9b2786
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-12-12 13:19:06 -0500

    removed padding from map calculations to use translate for positioning

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
   .then(data => createHeatMap(data))
 
-const width = 1300;
-const height = 550;
-const wPadding = 100;
-const hPadding = 40;
+const padding = 60;
+const width = 1300 - padding;
+const height = 550 - padding;
 
 function createHeatMap(data) {
   const { baseTemperature, monthlyVariance } = data;
@@ -15,16 +14,16 @@ function createHeatMap(data) {
   const minTemp = (baseTemperature + d3.min(monthlyVariance, d => d.variance)).toFixed(1);
   const maxTemp = (baseTemperature + d3.max(monthlyVariance, d => d.variance)).toFixed(1);
 
-  const cellHeight = (height - (hPadding * 2)) / 12;
-  const cellWidth = (width - wPadding) / (maxYear - minYear);
+  const cellHeight = height / 12;
+  const cellWidth = width / (maxYear - minYear);
 
   const xScale = d3.scaleTime()
                     .domain([new Date(minYear), new Date(maxYear)])
-                    .range([wPadding, width - wPadding])
+                    .range([0, width])
 
   const yScale = d3.scaleBand()
                     .domain(d3.range(12))
-                    .range([hPadding, height - hPadding])
+                    .range([0, height])
 
   const fillScale = d3.scaleSequential(d3.interpolateRdYlBu)
                       .domain([maxTemp, minTemp])
@@ -34,10 +33,14 @@ function createHeatMap(data) {
 
   const heatMap = d3.select("#heat-map")
                     .append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
+                    .attr("width", width + padding)
+                    .attr("height", height + padding)
 
-  heatMap.selectAll("rect")
+  const map = heatMap.append("g")
+                      .attr("width", width)
+                      .attr("height", height)
+
+  map.selectAll("rect")
           .data(monthlyVariance)
           .enter()
           .append("rect")
@@ -51,6 +54,7 @@ function createHeatMap(data) {
           .attr("data-temp", d => baseTemperature + d.variance)
           .attr("fill", d => fillScale(baseTemperature + d.variance))
 
+  map.attr("transform", `translate(${padding}, 0)`)
 
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
@@ -60,17 +64,13 @@ function createHeatMap(data) {
 
   heatMap.append("g")
           .attr("id","x-axis")
-          .attr("transform", `translate(0, ${height - hPadding})`)
+          .attr("transform", `translate(${padding}, ${height})`)
           .call(xAxis)
 
   heatMap.append("g")
           .attr("id", "y-axis")
-          .attr("transform", `translate(${wPadding}, 0)`)
+          .attr("transform", `translate(${padding}, 0)`)
           .call(yAxis)
-
-  // const legend = tempColors.map((color, i) => {
-  //   return { color: color, temp: ((i+1) * rangeLength).toFixed(1)}
-  // })
 
   const range = d3.range(minTemp, maxTemp, 1);
 
@@ -78,14 +78,14 @@ function createHeatMap(data) {
                    .append("svg")
                    .attr("width", width/3)
                    .attr("height", 100)
-
+  const legendWidth = width/3
   legend.selectAll("rect")
         .data(range)
         .enter()
         .append("rect")
         .attr("height", 25)
-        .attr("width", d => (width/3)/range.length)
-        .attr("x", (d,i) => i * ((width/3)/range.length))
+        .attr("width", d => legendWidth/range.length)
+        .attr("x", (d,i) => i * (legendWidth/range.length))
         .attr("y", 0)
         .attr("fill", d => fillScale(d))
 
@@ -98,9 +98,9 @@ function createHeatMap(data) {
   // colorAxis.ticks(range.length)
   legend.append("g")
         .attr("id", "legend-axis")
-        .attr("transform","translate(0, 25)")
+        .attr("transform",`translate(0, 25)`)
         .call(colorAxis)
 
-  legend.attr("transform",`translate(${wPadding}, 0)`)
+  legend.attr("transform",`translate(${padding}, 0)`)
 }
 
